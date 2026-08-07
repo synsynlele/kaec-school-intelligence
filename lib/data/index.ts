ddcf382a36b234ec3d38b473de0439f79219a01e
synsynlele <synsynlele@gmail.com>
2026-08-07 12:45:51 +0100

    Export KSI persistence services

---
+++ lib/data/index.ts
@@ -0,0 +1,6 @@
+export * from "./ai-runs";
+export * from "./artifact-version";
+export * from "./assessments";
+export * from "./diagnoses";
+export * from "./evidence";
+export * from "./lessons";
