e6208e39eee8ce7cd5f0713c36539d2ed14e8820
synsynlele <synsynlele@gmail.com>
2026-08-17 06:26:17 +0100

    feat: add curriculum library to Student KSI learning

--- app/student/learning/page.tsx
+++ app/student/learning/page.tsx
@@ -1,5 +1,11 @@
+import { StudentCurriculumLibraryPanel } from "@/components/student/student-curriculum-library-panel";
 import { StudentLearningLibrary } from "@/components/student/student-learning-library";
 
 export default function StudentLearningPage() {
-  return <StudentLearningLibrary />;
+  return (
+    <>
+      <StudentLearningLibrary />
+      <StudentCurriculumLibraryPanel />
+    </>
+  );
 }
