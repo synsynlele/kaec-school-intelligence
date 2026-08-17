4da23bdb3a0bd90203bae96c4963460198f68e32
synsynlele <synsynlele@gmail.com>
2026-08-17 12:47:14 +0100

    Expose Academic Resources in Teacher KSI

---
+++ app/teacher/resources/page.tsx
@@ -0,0 +1,5 @@
+import { AcademicResourcesClient } from "@/components/resources/academic-resources-client";
+
+export default function TeacherResourcesPage() {
+  return <AcademicResourcesClient />;
+}
