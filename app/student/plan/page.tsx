bf34b253e11b264fa6d7c17d90194c17112078b3
synsynlele <synsynlele@gmail.com>
2026-08-17 06:23:28 +0100

    feat: expose personalized student learning plan route

---
+++ app/student/plan/page.tsx
@@ -0,0 +1,5 @@
+import { StudentLearningPlanClient } from "@/components/student/student-learning-plan-client";
+
+export default function StudentLearningPlanPage() {
+  return <StudentLearningPlanClient />;
+}
