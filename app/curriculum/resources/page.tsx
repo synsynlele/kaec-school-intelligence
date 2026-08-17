39164546c8e6b2f7a41b9c6f7ab589bf720b510a
synsynlele <synsynlele@gmail.com>
2026-08-17 06:25:29 +0100

    feat: expose curriculum resource factory route

---
+++ app/curriculum/resources/page.tsx
@@ -0,0 +1,5 @@
+import { CurriculumResourceFactoryClient } from "@/components/curriculum/curriculum-resource-factory-client";
+
+export default function CurriculumResourceFactoryPage() {
+  return <CurriculumResourceFactoryClient />;
+}
