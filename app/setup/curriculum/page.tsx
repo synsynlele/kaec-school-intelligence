e94ba119c017d627fa22ae55f1867187ea317dce
synsynlele <synsynlele@gmail.com>
2026-08-17 06:25:46 +0100

    feat: connect curriculum resource factory

--- app/setup/curriculum/page.tsx
+++ app/setup/curriculum/page.tsx
@@ -1,5 +1,11 @@
+import { CurriculumResourceShortcut } from "@/components/curriculum/curriculum-resource-shortcut";
 import { CurriculumIntelligenceClient } from "@/components/workspace/curriculum-intelligence-client";
 
 export default function CurriculumIntelligencePage() {
-  return <CurriculumIntelligenceClient />;
+  return (
+    <>
+      <CurriculumResourceShortcut />
+      <CurriculumIntelligenceClient />
+    </>
+  );
 }
