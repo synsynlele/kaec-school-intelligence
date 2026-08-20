22923a436fcc0011f0daa79c1ab0b2ac66d14ad8
synsynlele <synsynlele@gmail.com>
2026-08-20 10:35:44 +0100

    Load structured workspace styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { KsiAppNav } from "@/components/navigation/ksi-app-nav";
 import { KsiSchoolShell } from "@/components/navigation/ksi-school-shell";
 import "./globals.css";
+import "./workspace-shell.css";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
